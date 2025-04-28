2d204211c4c5b58d3ae9335b309f8cbf9914fbe2
l-isaro <l.isaro@alustudent.com>
2025-04-28 15:57:17 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
